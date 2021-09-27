d78d43c00d109e801c69bee80323c97833ae7d57
Cesar Monroy <cesar.monroy.a@gmail.com>
2021-09-27 15:45:34 -0300

    avance

---
+++ src/test/java/cl/ciisa/api/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ciisa.api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
